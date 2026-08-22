9fcd0c98a0f8a4d70a535ad5a617aab47eda9706
Jit2005git <jitghosh304@gmail.com>
2026-08-22 20:46:54 +0530

    02v

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,4 +4,8 @@ var accountPassword = "12345";
 let accountCity = "Kolkata";
 
 // accountId = 9 //not allowed because accountId is a constant
+accountEmail = "hchc@gmail.com"
+accountPassword = "123456"
+accountCity = "Delhi"
 console.log(accountId);
+console.table({accountEmail, accountPassword, accountCity});
