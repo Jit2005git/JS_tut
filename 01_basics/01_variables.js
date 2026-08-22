320ce63c74077f5efe51e7814d833c6937c334ed
Jit2005git <jitghosh304@gmail.com>
2026-08-22 19:17:47 +0530

    01v

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 123456789;
+let accountEmail ="jit@google.com"
+var accountPassword = "12345";
+let accountCity = "Kolkata";
+
+// accountId = 9 //not allowed because accountId is a constant
+console.log(accountId);
