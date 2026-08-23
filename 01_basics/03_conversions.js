b2bd29989cc4357d3554d2d9d86c7ffb3754c710
Jit2005git <jitghosh304@gmail.com>
2026-08-24 01:15:03 +0530

    5con

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -1,6 +1,7 @@
-let score="33"
+let score="33aa"
 console.log(typeof score); //string
 console.log(typeof(score));
 
 let valueInNumber= Number(score)
 console.log(typeof valueInNumber); //number
+console.log(valueInNumber); //33
