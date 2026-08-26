425e7b1911e9621ded6123955e4538a9504bbce0
Jit2005git <jitghosh304@gmail.com>
2026-08-26 12:59:25 +0530

    c1

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+  
+
+
+  console.log(undefined==0); // false
+  console.log(undefined==null); // true 
+  console.log(undefined<0); // false
+
+  console.log(null==0); // false
+  console.log(null>0); // false
+  console.log(null<0); // false 
